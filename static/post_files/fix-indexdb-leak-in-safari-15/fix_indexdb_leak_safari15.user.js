65c470a9bcdbc699d1ec592aa6e2d8f89092381e
Manual <am@catto.io>
2022-01-16 15:57:28 +0200

    fix bugs

--- static/post_files/fix-indexdb-leak-in-safari-15/fix_indexdb_leak_safari15.user.js
+++ static/post_files/fix-indexdb-leak-in-safari-15/fix_indexdb_leak_safari15.user.js
@@ -2,11 +2,12 @@
 // @name        Fix indexedDB leak
 // @description Fixes the Safari 15 indexedDB database leaking by removing the databases() function.
 // @author      Manual
-// @version     1.1
+// @version     1.2
 // @updateURL   https://catto.io/post_files/fix-indexdb-leak-in-safari-15/fix_indexdb_leak_safari15.user.js
 // @downloadURL https://catto.io/post_files/fix-indexdb-leak-in-safari-15/fix_indexdb_leak_safari15.user.js
-// @match       *://*/*
-// @run-at      document-start
+// @include     *
 // ==/UserScript==
 
 window.indexedDB.databases = undefined
+globalThis.indexedDB.databases = undefined
+indexedDB.databases = undefined
